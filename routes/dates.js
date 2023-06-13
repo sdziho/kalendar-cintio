89b884bc9aa0898cbabc89761dccb57883d80158
Sadzid <sadza.fb@gmail.com>
2023-06-13 14:13:01 +0200

    delete dates

--- routes/dates.js
+++ routes/dates.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 
-const { setDates, getDates } = require("../utils/Dates");
+const { setDates, getDates, deleteDates } = require("../utils/Dates");
 const { userAuth } = require("../utils/Auth");
 
 router.post("/set-dates", userAuth, async (req, res) => {
@@ -8,6 +8,9 @@ router.post("/set-dates", userAuth, async (req, res) => {
 });
 router.get("/get-dates", userAuth, async (req, res) => {
   await getDates(req, res);
+});
+router.post("/delete-dates", userAuth, async (req, res) => {
+  await deleteDates(req, res);
 });
 
 module.exports = router;
--- utils/Dates.js
+++ utils/Dates.js
@@ -48,8 +48,46 @@ const getDates = async (req, res) => {
     });
   }
 };
+const deleteDates = async (req, res) => {
+  try {
+    const user = serializeUser(req.user);
+    //const dates = await Date.findOne({ userID: user._id });
+    const myDates = await Date.findOne({ userID: user._id });
+
+    let exists = myDates.dates.some((obj) => {
+      return obj._id == req.body._id;
+    });
+
+    const daysLeft = myDates.freeDays + req.body.deletedDates;
+    if (exists) {
+      await Date.findOneAndUpdate(
+        { userID: user._id },
+        {
+          $pull: { dates: { _id: req.body._id } },
+          $set: { freeDays: daysLeft },
+        },
+        { returnOriginal: false }
+      );
+      return res.status(201).json({
+        message: "Dates successfully deleted.",
+        success: true,
+      });
+    } else {
+      return res.status(500).json({
+        message: "ID doesn't exists",
+        success: false,
+      });
+    }
+  } catch (err) {
+    return res.status(500).json({
+      message: "Unable To Finish",
+      success: false,
+    });
+  }
+};
 
 module.exports = {
   setDates,
   getDates,
+  deleteDates,
 };
